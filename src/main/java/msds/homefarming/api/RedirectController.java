97595a59aece6cbac16a527a8264fd4bb78063cc
doubleDRG <dyl660115@gmail.com>
2023-12-09 11:31:02 +0900

    [fix] 새로고침 이슈 => 홈이 아닌 현재화면으로 새로고침

--- src/main/java/msds/homefarming/api/RedirectController.java
+++ src/main/java/msds/homefarming/api/RedirectController.java
@@ -3,56 +3,55 @@ package msds.homefarming.api;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-//==FE의 챗봇화면, 일지화면, .. 등에서 새로고침 시 404안뜨도록 리다이렉트==//
 @Controller
 public class RedirectController
 {
     @GetMapping("/diag-ai")
     public String diagAiRedirect()
     {
-        return "redirect:/";
+        return "index.html";
     }
 
     @GetMapping("/dict")
     public String dictRedirect()
     {
-        return "redirect:/";
+        return "index.html";
     }
 
     @GetMapping("/diary")
     public String diaryRedirect()
     {
-        return "redirect:/";
+        return "index.html";
     }
 
     @GetMapping("/diary/*")
     public String diaryDetailRedirect()
     {
-        return "redirect:/";
+        return "index.html";
     }
 
     @GetMapping("/diary/edit")
     public String diaryEditRedirect()
     {
-        return "redirect:/";
+        return "index.html";
     }
 
     @GetMapping("/chatbot")
     public String chatbotRedirect()
     {
-        return "redirect:/";
+        return "index.html";
     }
 
     @GetMapping("/my-info")
     public String myInfoRedirect()
     {
-        return "redirect:/";
+        return "index.html";
     }
 
     @GetMapping("/my-info/*")
     public String myInfoEtcRedirect()
     {
-        return "redirect:/";
+        return "index.html";
     }
 
 }
